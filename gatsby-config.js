4faf4df9855e5c6bd59596f9dfea6fc46350225e
Grayson Hicks <graysonhicks@gmail.com>
2021-07-09 09:54:55 -0400

    flags

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
   siteMetadata: {
     title: `Creative Portfolio`,
   },
+  flags: {
+    PRESERVE_FILE_DOWNLOAD_CACHE: true,
+    PRESERVE_WEBPACK_CACHE: true,
+    DEV_SSR: false,
+    FAST_DEV: false,
+    PARALLEL_SOURCING: true,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
